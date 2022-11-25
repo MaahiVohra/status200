749686c3f4201cc396e44d0cf73d167591707a70
MaahiVohra <vohramaahi@gmail.com>
2022-11-25 11:05:07 +0530

    web3: alchemy added

--- client/components/home/TweetBox.tsx
+++ client/components/home/TweetBox.tsx
@@ -5,7 +5,11 @@ import { RiFileGifLine, RiBarChartHorizontalFill } from "react-icons/ri";
 import { IoMdCalendar } from "react-icons/io";
 import { MdOutlineLocationOn } from "react-icons/md";
 import { client } from "../../lib/client";
-
+import { paymentContractAddress } from "../../lib/constants";
+const abi = require("../../lib/constants");
+const paymentContractABI = abi.paymentContractABI;
+import Web3 from "web3";
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const style = {
 	wrapper: `px-4 flex flex-row  pb-4`,
 	tweetBoxLeft: `mr-4`,
@@ -24,10 +28,77 @@ function TweetBox() {
 	const [tweetMessage, setTweetMessage] = useState("");
 	const { currentAccount, fetchTweets, currentUser } =
 		useContext(TwitterContext);
-
 	const submitTweet = async (event: any) => {
 		event.preventDefault();
+		var to = paymentContractAddress;
+		const web3 = createAlchemyWeb3(
+			"https://eth-goerli.g.alchemy.com/v2/khrf9mtb_KAZ5uOXCqiJ4Gxvvckh7dFl"
+		);
+		// let web3 = new Web3(
+		// 	new Web3.providers.HttpProvider(
+		// 		"https://eth-goerli.g.alchemy.com/v2/khrf9mtb_KAZ5uOXCqiJ4Gxvvckh7dFl"
+		// 	)
+		// );
+		let mycontract = new web3.eth.Contract(
+			paymentContractABI,
+			paymentContractAddress
+		);
+		const accurateTime = new Date(Date.now()).toISOString();
 
+		try {
+			await mycontract.methods
+				.sendMessage(to, tweetMessage, accurateTime)
+				.send({ from: currentAccount });
+		} catch (err) {
+			console.log("catch working");
+			return;
+		}
+
+		// .send({ from: myaccount }), function (err, transcationHash) {
+		// 	if (err) {
+		// 		console.log(error);
+		// 	} else {
+		// 		var decoration =
+		// 			'<div class="row justify-content-start">' +
+		// 			'<div class="col-4 col-md-auto">' +
+		// 			'<div class="alert alert-primary" role="alert">' +
+		// 			message +
+		// 			'<h6 style="font-size: 0.6em;">send ' +
+		// 			accurateTime +
+		// 			"</h6>" +
+		// 			"</div>" +
+		// 			"</div>" +
+		// 			"</div>";
+		// 		var buttonText = document.getElementById(
+		// 			"groupinputbutton" + to
+		// 		).innerHTML;
+		// 		document
+		// 			.getElementById("groupinputbutton" + to)
+		// 			.remove();
+		// 		//document.getElementById("button"+det.id).remove();
+		// 		document.getElementById(
+		// 			"messagepart" + to
+		// 		).innerHTML += decoration;
+		// 		document.getElementById(
+		// 			"messagepart" + to
+		// 		).innerHTML +=
+		// 			'<div class="input-group mb-0" id="groupinputbutton' +
+		// 			to +
+		// 			'">' +
+		// 			'<input type="text" class="form-control" placeholder="Type message" id="inputbutton' +
+		// 			to +
+		// 			'"aria-label="Recipien username" aria-describedby="button-addon2" />' +
+		// 			'<button class="btn btn-warning" type="button" onclick=onSend(this) id="button' +
+		// 			to +
+		// 			'" style="padding-top: .55rem;">' +
+		// 			"Send" +
+		// 			"</button>" +
+		// 			"</div>";
+		// 		document
+		// 			.getElementById("groupinputbutton" + to)
+		// 			.scrollIntoView();
+		// 	}
+		// });
 		if (!tweetMessage) return;
 		const tweetId = `${currentAccount}_${Date.now()}`;
 
@@ -102,6 +173,7 @@ function TweetBox() {
 						>
 							Post
 						</button>
+						<div>Post cooldown:</div>
 					</div>
 				</form>
 			</div>
